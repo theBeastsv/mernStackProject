08def9762faae52c5cae12f37066d394e10a0af7
TheBeastSv <veerverma3372@gmail.com>
2024-04-07 16:47:44 +0530

    job code

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,8 +1,8 @@
-import { catchAsyncError } from "./catchAsyncError.js";
-import { ErrorHandler } from "./error";
+import { catchAsyncErrors } from "./catchAsyncError.js";
+import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/userModel.js";
-export const isauthorized = catchAsyncError(async (req, res, next) => {
+export const isauthorized = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
     return next(new ErrorHandler("User not Authorized ", 400));
--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,5 +1,22 @@
 import express from "express";
+import {
+  deleteJob,
+  getAllJobs,
+  getMyJobs,
+  getSingleJob,
+  postJob,
+  updateJob,
+} from "../controllers/jobControllers.js";
+
+import { isauthorized } from "../middlewares/auth.js";
 
 const router = express.Router();
+
+router.get("/getall", getAllJobs);
+router.post("/post", isauthorized, postJob);
+router.get("/getmyjobs", isauthorized, getMyJobs);
+router.put("/update/:id", isauthorized, updateJob);
+router.delete("/delete/:id", isauthorized, deleteJob);
+router.get("/:id", isauthorized, getSingleJob);
 
 export default router;
