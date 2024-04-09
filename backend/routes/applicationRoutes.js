b749fb0e243757e39318150dc9d6dcd3f3fe0b0d
TheBeastSv <veerverma3372@gmail.com>
2024-04-09 11:38:49 +0530

    application

--- backend/routes/applicationRoutes.js
+++ backend/routes/applicationRoutes.js
@@ -1,5 +1,17 @@
 import express from "express";
+import {
+  employerGetAllApplications,
+  jobseekerDeleteApplication,
+  jobseekerGetAllApplications,
+  postApplication,
+} from "../controllers/applicationControllers.js";
+import { isauthorized } from "../middlewares/auth.js";
 
 const router = express.Router();
+
+router.post("/post", isauthorized, postApplication);
+router.get("/employer/getall", isauthorized, employerGetAllApplications);
+router.get("/jobseeker/getall", isauthorized, jobseekerGetAllApplications);
+router.delete("/delete/:id", isauthorized, jobseekerDeleteApplication);
 
 export default router;
